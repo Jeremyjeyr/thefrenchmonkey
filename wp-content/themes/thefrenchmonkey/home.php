d8f463b307346bd559d2404eab131f2fb9d1da76
Jérémy <jp.jeremypichard@gmail.com>
2017-11-27 10:58:59 +0100

    update home.php

--- wp-content/themes/thefrenchmonkey/home.php
+++ wp-content/themes/thefrenchmonkey/home.php
@@ -1,3 +1,4 @@
+<?php /* Template Name: Home*/?>
 <?php get_header(); //appel du template header.php  ?>
 <div class="container">
 <section>
@@ -26,11 +27,22 @@ array(
   'field'    => 'slug',
   'terms'    => 'vimeo'
 ),
-));
+)
+);
+$args4 = array(
+'post_type' => 'freebies',
+'posts_per_page' => 1
+);
+$args5 = array(
+'post_type' => 'bundles',
+'posts_per_page' => 1
+);
 // The Query
 $everyday = new WP_Query( $args1 );
 $animation = new WP_Query( $args2 );
 $animation16_9 = new WP_Query( $args3 );
+$freebies = new WP_Query( $args4 );
+$bundles = new WP_Query( $args5 );
 
 // Render of the day -> Everyday
 if ( $everyday->have_posts() ) {
@@ -42,33 +54,6 @@ if ( $everyday->have_posts() ) {
     ?>
 
     <article>
-     <h2><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">[<?php echo $date->format('d-m-y'); ?>]-<?php the_field('title'); ?></a></h2>
-
-     <!-- <?php // Affichage des logiciels utilisés
-     $terms = get_field('software');
-     if( $terms ): ?>
-
-       <ul>
-       <?php foreach( $terms as $term ): ?>
-         <p><?php echo $term->name; ?></p>
-       <?php endforeach; ?>
-       </ul>
-
-     <?php endif; ?>
-
-
-     <?php // Affichage du renderer utilisé
-     $terms = get_field('renderer');
-     if( $terms ): ?>
-
-       <ul>
-       <?php foreach( $terms as $term ): ?>
-         <p><?php echo $term->name; ?></p>
-       <?php endforeach; ?>
-       </ul>
-
-     <?php endif; ?> -->
-
 
      <?php // Affichage de l'image
 
@@ -85,7 +70,7 @@ $file = get_field('project_file');
 
 if( $file ): ?>
 
-	<a href="<?php echo $file['url']; ?>"><?php echo $file['filename']; ?>[<?php echo $date->format('d-m-y'); ?>]</a>
+	<h2><a href="<?php echo $file['url']; ?>"><?php echo $file['filename']; ?>[<?php echo $date->format('d-m-y'); ?>]-<?php the_field('title');?></a></h2>
 
 <?php endif; ?>
    </article>
@@ -109,8 +94,7 @@ if( $file ): ?>
        // TO DO : A trier selon animation carré ou pas
     ?>
     <?php $animation_url = get_field('animation_url'); ?>
-      <iframe class="col-sm-10" src="<?php echo $animation_url ?>" width="380" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
-
+      <iframe class="col-sm-3" src="<?php echo $animation_url ?>" width="380" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
     <?php
   }
    /* Restore original Post Data */
@@ -119,6 +103,72 @@ if( $file ): ?>
   // no posts found
   }
   ?>
+  <?php
+  // animation
+  if ( $animation16_9->have_posts() ) {
+   while ( $animation16_9->have_posts() ) {
+     $animation16_9->the_post();
+     // TO DO : A trier selon animation carré ou pas
+  ?>
+  <?php $animation_url = get_field('animation_url'); ?>
+  <iframe class="col-sm-6" src="<?php echo $animation_url ?>" width="380" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allowFullScreen="true"></iframe>
+
+  <?php
+}
+ /* Restore original Post Data */
+            wp_reset_postdata();
+} else {
+// no posts found
+}
+?>
+</section>
+</div>
+<div class="container">
+<section>
+<?php
+// animation
+if ( $freebies->have_posts() ) {
+ while ( $freebies->have_posts() ) {
+   $freebies->the_post();
+   // TO DO : A trier selon animation carré ou pas
+?>
+<?php // Affichage de l'image
+
+$image = get_field('image');
+
+if( !empty($image) ): ?>
+
+  <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>"/>
+
+<?php endif; ?>
+<?php
+}
+/* Restore original Post Data */
+          wp_reset_postdata();
+} else {
+// no posts found
+}
+?>
+
+<h3><?php the_field('h3') ?></h3>
+<p><?php the_field('p') ?></p>
+<h3><?php the_field('h3_2') ?></h3>
+
+  </section>
+</div>
+<div class="container">
+<section>
+  <?php // Affichage de l'image
+
+  $imagephoto = get_field('image_photo');
+
+  if( !empty($imagephoto) ): ?>
+  <img src="<?php echo $imagephoto['url']; ?>" alt="<?php echo $imagephoto['alt']; ?>"/>
+<?php endif; ?>
+
+<h3><?php the_field('h3_3') ?></h3>
+<p><?php the_field('p2') ?></p>
+
   </section>
 </div>
 <?php get_footer(); //appel du template footer.php ?>
